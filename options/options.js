0a77909933593846b3da37e7879e156528a95707
Crowdbotics <team@crowdbotics.com>
2023-05-17 00:40:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "still-recipe-40754",
+  url: "https://still-recipe-40754.botics.co",
+  api: "https://still-recipe-40754.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
